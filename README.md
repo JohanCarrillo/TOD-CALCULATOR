06040b3264684fdd107626123c7b8861f7879d11
JohanCarrillo <johanime1907@gmail.com>
2021-12-28 10:30:57 -0500

    documentation is up to date and the code has been commented.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # TOD-CALCULATOR
+This calculator was made as the final project of the javascript module in The 
+Odin Project.
+
+## How to use:
+The calculator executes the inputs one by one, using the result of every previous
+operation as the first number in the next operation. It doesn't care about the
+symbol hierarchy, everything flows from left to right. For example, the operation
+1 + 2 * 6 / 3 => [(1+2) * 6] / 3 = 6.
+
+### The special keys:
+**AC**: stands for All Clear, this button deletes all the input history in the calculator
+**DEL**: stands for DELete, it deletes the last input
+All the other key are the current numbers, operators and the floating decimal point.
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // --------------------------- OPERATIONS -------------------------------------
 function operate(symbol, number1, number2) {
+  // this function executes the basic operations
+
   switch (symbol) {
     case "+": return number1 + number2;
     case "-": return number1 - number2;
@@ -12,20 +14,29 @@ function operate(symbol, number1, number2) {
 // ----------------------------------------------------------------------------
 
 let result;
-const historyArray = [];
-const classArray = [];
+const historyArray = [];  // store every input of the calculator
+const classArray = [];  // store class names of every input
 
 const displayUp = function(keyToShow) {
+  // used to show the input history in the small-upper screen
+
   upper = document.querySelector('#upper');
   upper.textContent = keyToShow.join('');
 }
 
 const displayBottom = function(keyToShow){
+  // used to show every result in the big-lower screen
+
   lower = document.querySelector('#lower');
   lower.textContent = keyToShow;
 }
 
 function calculate(historyArray, classArray) {
+  /* This function receives all the inputs and its classes and returns the 
+      final result rounded to 5 decimals executing operations one by one from 
+      left to right in the input order. If there is an invalid input returns 
+      an error message */
+
   let numberString = ''; 
   const numbers = [];
   const operators = [];
@@ -33,10 +44,10 @@ function calculate(historyArray, classArray) {
   
   for (let i=0; i<historyArray.length; i++) {
 
-    if (classArray[i] === 'number') {
+    if (classArray[i] === 'number') {  // chain a number 
       numberString += historyArray[i];
-    } else {  // when it finds an operator
-      if (numberString !== '') {
+    } else {  // when it finds an operator saves the number and the operator
+      if (numberString !== '') {  // this is in case the first input is not a number
         numbers.push(Number(numberString));  // save the number
       } else {
         return 'Invalid Operation';
@@ -46,25 +57,24 @@ function calculate(historyArray, classArray) {
     }
   }
   if (numberString !== '') numbers.push(Number(numberString));  // save last number
-  // until here it saves numbers and operators in separate list
-  
 
-  if (operators.length !== 0) {
+  if (operators.length !== 0) {  // if there is at least one input operator
     let number1 = numbers[0];
-    for (let j=0; j<operators.length; j++) {
+    for (let j=0; j<operators.length; j++) {  // runs over all the operators
       result = operate(operators[j], number1, numbers[j+1]);
-      number1 = result;
+      number1 = result;  // the next operation uses the previous result
     }
-  } else {  // when there was noot any operator, only one number
-    if (!isNaN(numbers[0])) return (Math.round(numbers.join('') * 100) / 100).toFixed(5);  // invalid number
-    else return 'Invalid Operation';
+  } else {  // when there is not any operator, only one number return that number
+    if (!isNaN(numbers[0])) return (Math.round(numbers.join('') * 100000) / 100000).toFixed(5);  // invalid number
+    else return 'Invalid Operation';  // in case the only input is an operator return error
   }
-  
-  if (!isNaN(result)) return (Math.round(result * 100) / 100).toFixed(5);
+  // this isNaN is for when the input was an invalid number like 1.1.0
+  if (!isNaN(result)) return (Math.round(result * 100000) / 100000).toFixed(5);
   else return 'Invalid Operation';
 }
 
 function clear() {
+  // deletes all the history, cleaning the display
   historyArray.length = 0;
   classArray.length = 0;
   result = '';
@@ -77,17 +87,19 @@ const buttons = Array.from(document.querySelectorAll('button'));
 
 // click event that actually saves the pressed key
 buttons.forEach(element => element.addEventListener('click', () => {
-  
+  /* This function assign to every button its function and saves the input history
+      and its class */
+
   switch (true) {
-    case element.id === 'EQU':  // calculate
-      // check if last element was not an operator
+    case element.id === 'EQU':  // '=' use the history to calculate
+      // check if last element input was not an operator
       if (classArray[classArray.length-1] === 'operator') {
         window.alert('Last element must be a number');
         clear();
         break;
       }
       result = calculate(historyArray, classArray);
-      // error when dividing by zero
+      // error when dividing by zero or invalid input
       if (result === 'Infinity' || result === 'Invalid Operation') {  
         window.alert('Invalid Operation');
         clear();
@@ -106,7 +118,7 @@ buttons.forEach(element => element.addEventListener('click', () => {
       displayUp(historyArray);
       break;
 
-    default:  // save numbers and operators
+    default:  // save numbers and operators | this is when a number or operator button is pressed
       historyArray.push(element.textContent);
       classArray.push(element.className);
       displayUp(historyArray);
